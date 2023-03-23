d6a2b280c5bda3eb77098aadbb89a332e396ed2d
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2023-03-23 09:44:09 -0400

    Update spectrogram.py

--- spectrogram.py
+++ spectrogram.py
@@ -21,7 +21,7 @@ def get_data(path, data_ch, start, stop, block=1):
     return data, fs
     
 
-def outlier_removal(data,  percentile_threshold=[1, 95]):
+def outlier_removal(data, percentile_threshold=[1, 95]):
     # remove outliers
     nan_indices = (data < np.percentile(data, percentile_threshold[0])) | (data > np.percentile(data, percentile_threshold[1]))
     data[nan_indices] = np.nan
@@ -54,75 +54,75 @@ if __name__ == '__main__':
     index = pd.read_csv(os.path.join(main_path, 'combined_index.csv'))
     index = index[~index['animal_id'].isin(['-mouse1,s5pos1-','-mouse2,s2pos1-', '-mouse2,s4pos2-']) ]
     
-    # map index frequencies to correct stim values
-    index = index[index['stim_hz']>4]
-    idx = (index['stim_hz']>=43) & (index['stim_hz']<=47)
-    index.loc[idx,'stim_hz'] = 45
-    idx = (index['stim_hz']>=58) & (index['stim_hz']<=61)
-    index.loc[idx,'stim_hz'] = 60
-    print(np.sort(index['stim_hz'].unique()))
+#     # map index frequencies to correct stim values
+#     index = index[index['stim_hz']>4]
+#     idx = (index['stim_hz']>=43) & (index['stim_hz']<=47)
+#     index.loc[idx,'stim_hz'] = 45
+#     idx = (index['stim_hz']>=58) & (index['stim_hz']<=61)
+#     index.loc[idx,'stim_hz'] = 60
+#     print(np.sort(index['stim_hz'].unique()))
     
-    # select slice for analysis
-    plot_index = index#[index['animal_id'].isin([#'-mouse1,s5pos1-', '-mouse1,s5pos2-',
-                                                 # '-mouse2,s2pos1-', '-mouse2,s2pos2-',
-                                                # '-mouse2,s4pos1-', '-mouse2,s4pos2-'
-                                                # ]) ]
+#     # select slice for analysis
+#     plot_index = index#[index['animal_id'].isin([#'-mouse1,s5pos1-', '-mouse1,s5pos2-',
+#                                                  # '-mouse2,s2pos1-', '-mouse2,s2pos2-',
+#                                                 # '-mouse2,s4pos1-', '-mouse2,s4pos2-'
+#                                                 # ]) ]
     
-    power_ratio = []
-    peak_freq = []
-    for i,row in tqdm(plot_index.iterrows(), total=len(plot_index)):
+#     power_ratio = []
+#     peak_freq = []
+#     for i,row in tqdm(plot_index.iterrows(), total=len(plot_index)):
         
-        # get data, remove outliers and obtain stft
-        row_path = os.path.join(main_path, row['folder_path'], row['file_name'])
+#         # get data, remove outliers and obtain stft
+#         row_path = os.path.join(main_path, row['folder_path'], row['file_name'])
 
         
-        # get stim
-        data, fs = get_data(row_path, 
-                            data_ch=row.channel_id,
-                            start=row['start_time'], 
-                            stop=row['stop_time'])
-        data = outlier_removal(data, percentile_threshold=outlier_threshold)
-        f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
-                              f_range=[row['stim_hz']-1, row['stim_hz']+1])
-        stim_power = np.mean(pmat)
+#         # get stim
+#         data, fs = get_data(row_path, 
+#                             data_ch=row.channel_id,
+#                             start=row['start_time'], 
+#                             stop=row['stop_time'])
+#         data = outlier_removal(data, percentile_threshold=outlier_threshold)
+#         f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
+#                               f_range=[row['stim_hz']-1, row['stim_hz']+1])
+#         stim_power = np.mean(pmat)
         
-        # get baseline
-        data, fs = get_data(row_path, 
-                            data_ch=row.channel_id,
-                            start=row['start_time'] - int(baseline_time*fs), 
-                            stop=row['start_time']-1)
-        data = outlier_removal(data, percentile_threshold=outlier_threshold)
-        f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
-                              f_range=[row['stim_hz']-1, row['stim_hz']+1])
-        base_power = np.mean(pmat)
+#         # get baseline
+#         data, fs = get_data(row_path, 
+#                             data_ch=row.channel_id,
+#                             start=row['start_time'] - int(baseline_time*fs), 
+#                             stop=row['start_time']-1)
+#         data = outlier_removal(data, percentile_threshold=outlier_threshold)
+#         f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
+#                               f_range=[row['stim_hz']-1, row['stim_hz']+1])
+#         base_power = np.mean(pmat)
         
-        # get peak freq during baseline
-        data, fs = get_data(row_path, 
-                            data_ch=row.channel_id,
-                            start=row['start_time'] - int(baseline_time*fs), 
-                            stop=row['start_time']-1)
-        data = outlier_removal(data, percentile_threshold=outlier_threshold)
-        f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
-                              f_range=f_range)
+#         # get peak freq during baseline
+#         data, fs = get_data(row_path, 
+#                             data_ch=row.channel_id,
+#                             start=row['start_time'] - int(baseline_time*fs), 
+#                             stop=row['start_time']-1)
+#         data = outlier_removal(data, percentile_threshold=outlier_threshold)
+#         f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
+#                               f_range=f_range)
        
-        psd = np.mean(pmat, axis=1)
-        plt.plot(f,psd)
-        idx = np.argmax(psd)
-        peak_freq.append(f[idx])
-        power_ratio.append(stim_power/base_power)
+#         psd = np.mean(pmat, axis=1)
+#         plt.plot(f,psd)
+#         idx = np.argmax(psd)
+#         peak_freq.append(f[idx])
+#         power_ratio.append(stim_power/base_power)
         
-    plot_index['power_ratio'] = power_ratio
-    plot_index['peak_freq'] = peak_freq
+#     plot_index['power_ratio'] = power_ratio
+#     plot_index['peak_freq'] = peak_freq
 
-plot_data = plot_index.groupby(['animal_id', 'stim_hz'], ).mean(numeric_only=True).reset_index()
-sns.set(font_scale=2)
-plt.figure()
-sns.lineplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar=None, hue='animal_id',  alpha=.5) #color='grey',
-sns.lineplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar='se',color ='black')
+# plot_data = plot_index.groupby(['animal_id', 'stim_hz'], ).mean(numeric_only=True).reset_index()
+# sns.set(font_scale=2)
+# plt.figure()
+# sns.lineplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar=None, hue='animal_id',  alpha=.5) #color='grey',
+# sns.lineplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar='se',color ='black')
 
-sns.relplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar='se', hue='animal_id', col='animal_id', kind='line')
+# sns.relplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar='se', hue='animal_id', col='animal_id', kind='line')
 
-sns.catplot(data=plot_data, x='animal_id', y='peak_freq', errorbar='se', kind='box')
+# sns.catplot(data=plot_data, x='animal_id', y='peak_freq', errorbar='se', kind='box')
         
         
 
