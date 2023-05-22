7ed488012f367e4125d6120ac155e86ac506c5ba
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2023-05-22 15:05:29 -0400

    Update spectrogram_eric.py

--- spectrogram_eric.py
+++ spectrogram_eric.py
@@ -48,77 +48,81 @@ if __name__ == '__main__':
     overlap = 0.5
     f_range = [10, 49]
     outlier_threshold = [0.18, 99.9]
-    baseline_time = 2
+    baseline_time = 30
     
     # get index
     index = pd.read_csv(os.path.join(main_path, 'combined_index.csv'), keep_default_na=False)
-
+    index = index.fillna('')
+                         
     # map index frequencies to correct stim values
     index['stim_hz'] = index.groupby('stim_hz', group_keys=False)['stim_hz'].apply(pd.cut, bins=[3, 7, 15, 23, 29, 33, 37, 41, 46, 51, 65, 74, 84, 94, 104], 
                                                                                  labels=[5, 10, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100]).astype(int)
+    # power_ratio = []
+    # for i,row in tqdm(index.iterrows(), total=len(index)):
 
+    #     # get data, remove outliers and obtain stft
+    #     row_path = os.path.join(main_path, row['folder_path'], row['file_name'])
 
-    power_ratio = []
-    peak_freq = []
-    for i,row in tqdm(index.iterrows(), total=len(index)):
-
-        # get data, remove outliers and obtain stft
-        row_path = os.path.join(main_path, row['folder_path'], row['file_name'])
-
+    #     # get stim
+    #     data, fs = get_data(row_path, 
+    #                         data_ch=row.channel_id,
+    #                         start=row['start_time'], 
+    #                         stop=row['stop_time'],
+    #                         block=row.block)
+    #     data = outlier_removal(data, percentile_threshold=outlier_threshold)
+    #     f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
+    #                           f_range=[row['stim_hz']-1, row['stim_hz']+1])
+    #     stim_power = np.mean(pmat)
         
-        # get stim
-        data, fs = get_data(row_path, 
-                            data_ch=row.channel_id,
-                            start=row['start_time'], 
-                            stop=row['stop_time'],
-                            block=row.block)
-        data = outlier_removal(data, percentile_threshold=outlier_threshold)
-        f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
-                              f_range=[row['stim_hz']-1, row['stim_hz']+1])
-        stim_power = np.mean(pmat)
-        
-        # get baseline
-        data, fs = get_data(row_path, 
-                            data_ch=row.channel_id,
-                            start=row['start_time'] - int(baseline_time*fs), 
-                            stop=row['start_time']-1,
-                            block=row.block)
-        data = outlier_removal(data, percentile_threshold=outlier_threshold)
-        f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
-                              f_range=[row['stim_hz']-1, row['stim_hz']+1])
-        base_power = np.mean(pmat)
-        
-        # get peak freq during baseline
-        data, fs = get_data(row_path, 
-                            data_ch=row.channel_id,
-                            start=row['start_time'] - int(baseline_time*fs), 
-                            stop=row['start_time']-1,
-                            block=row.block)
-        data = outlier_removal(data, percentile_threshold=outlier_threshold)
-        f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
-                              f_range=f_range)
-       
-        psd = np.mean(pmat, axis=1)
-        # plt.plot(f,psd, color='gray')
-        idx = np.argmax(psd)
-        peak_freq.append(f[idx])
-        power_ratio.append(stim_power/base_power)
-        
-    index['power_ratio'] = power_ratio
-    index['peak_freq'] = peak_freq
+    #     # get baseline
+    #     data, fs = get_data(row_path, 
+    #                         data_ch=row.channel_id,
+    #                         start=row['start_time'] - int(baseline_time*fs), 
+    #                         stop=row['start_time']-1,
+    #                         block=row.block)
+    #     data = outlier_removal(data, percentile_threshold=outlier_threshold)
+    #     f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
+    #                           f_range=[row['stim_hz']-1, row['stim_hz']+1])
+    #     base_power = np.mean(pmat)
+    #     power_ratio.append(stim_power/base_power)  
+    # index['power_ratio'] = power_ratio
 
-plot_data = index.groupby(['animal_id', 'stim_hz', 'brain_region'], ).mean(numeric_only=True).reset_index() #'laser',
-plot_data['power_ratio'] = plot_data['power_ratio'].astype(float)
-sns.set(font_scale=2)
-plt.figure()
-# sns.lineplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar=None, style='animal_id', color='grey', alpha=.5, legend=False) #
-# sns.lineplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar='se',color ='black')
 
-# g = sns.relplot(data=index, x='stim_hz', y='power_ratio', errorbar='se', hue='animal_id',
-#              row='brain_region', kind='line') #col='laser',
-# for ax in g.axes.flatten():
-#         ax.axhline(1, linestyle = '--', color = 'gray')   
-# sns.relplot(data=plot_data, x='stim_hz', y='power_ratio', errorbar='se', hue='animal_id', col='animal_id', kind='line')
+# sns.catplot(data=index, x='treatment', y='power_ratio', kind='bar', errorbar='se') #
 
-sns.catplot(data=plot_data, x='animal_id', y='peak_freq', errorbar='se', kind='box')
+
+fs = int(index.sampling_rate.iloc[0])
+# add baseline
+base_index = index.copy()
+stim_index = index.copy()
+base_index['condition'] = 'pre'
+stim_index['condition'] = 'stim' 
+base_index['start_time'] = stim_index['start_time'] - baseline_time*fs - 1
+base_index['stop_time'] = stim_index['start_time'] - fs - 1
+# stim_index = stim_index[stim_index['start_time'] > 5*60*fs]
+index = pd.concat([base_index, stim_index]).reset_index(drop=True)
+
+power_list = []
+for i,row in tqdm(index.iterrows(), total=len(index)):
+
+    # get data, remove outliers and obtain stft
+    row_path = os.path.join(main_path, row['folder_path'], row['file_name'])
+
+
+    # get stim
+    data, fs = get_data(row_path, 
+                        data_ch=row.channel_id,
+                        start=row['start_time'], 
+                        stop=row['stop_time'],
+                        block=row.block)
+    data = outlier_removal(data, percentile_threshold=outlier_threshold)
+    f, t, pmat = get_stft(data, fs, win=win, overlap=overlap, 
+                          f_range=[row['stim_hz']-2, row['stim_hz']+2])
+    power = np.mean(pmat)
+    power_list.append(power)
+index['power'] = power_list    
+    
+
+sns.catplot(data=index, x='stop_time', y='power', hue='condition', kind='bar', errorbar='se') #
+
         
